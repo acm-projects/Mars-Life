c2a14e847ac57a7addc11a56c0c92b0090a84b05
sydney k <sydneykham@cometnet-10-21-49-54.utdallas.edu>
2021-10-15 13:11:31 -0500

    loading screen update

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SplashLoad from './components/SplashLoad';
+import { useState, useEffect } from 'react';
+
 
 export default function App() {
+  
+  
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 3000);
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {show ?<Text style={styles.loadText}>Finished loading!</Text> : <SplashLoad /> }
       <StatusBar style="auto" />
     </View>
   );
+  
+  
+
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#080B1F',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  loadText: {
+    color: "#FFFFFF"
+  }
 });
+
