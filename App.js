a0d06aa4ac00cb7664af0a4cb0005c91802d03e8
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-10-20 18:16:09 -0500

    a

--- App.js
+++ App.js
@@ -19,22 +19,53 @@ export default class weather extends React.Component{
     <ImageBackground
       style = {style.background}
       source = {require('./assets/HomeBack.png')}>
+      <ScrollView>
       <View style = {style.body}>
         <View style = {style.header}>
           <Text style = {style.headerText}> Weather</Text>
         </View>
-        <Image
+
+          <Image
             style = {style.mars}
             source = {require('./assets/Mars.png')}></Image>
-        <View style = {style.sol}>
-          <Text style = {style.solText}> Sol xxxx</Text>
-        </View>
+          <View style = {style.container}>
+            <View style = {style.solHeader}>
+              <Text style = {style.solText}> Sol XXXX  </Text>
+            </View>
+            <View style = {style.line}></View>
+            <View style = {style.containerInner}>
+              <Text style = {style.innerText}>Minimum and Maximum temperature: -xx to -xx </Text>
+            </View>
+            <View style = {style.containerInner}>
+              <Text style = {style.innerText}>Atmospheric Temperature: -xx to -xx</Text>
+            </View>
+            <View style = {style.containerInner}>
+              <Text style = {style.innerText}>Ground Temperature: -xx to -xx </Text>
+            </View>
+            <View style = {style.containerInner}>
+              <Text style = {style.innerText}>Conditions: Sunny</Text>
+            </View>
+            <View style = {style.containerInner}>
+              <Text style = {style.innerText}>Sunrise/Sunset time: idk what format it's in</Text>
+            </View>
+            <View style = {style.containerInner}>
+              <Text style = {style.innerText}>Ultraviolet radiation levels: idk how it's reported</Text>
+            </View>
+          </View>
+
+          <View style = {style.date}>
+            <Text style = {style.dateText}> Oct. 20, 2021</Text>
+          </View>
+
+
+
 
 
 
 
 
       </View>
+      </ScrollView>
     </ImageBackground>
 
    );
@@ -63,24 +94,85 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     alignSelf: 'center',
     resizeMode: 'contain',
-
+    height : '20%',
+    width: '20%'
+    
   },
-  sol: {
-    marginTop: '14',
-    width: '40%',
-    height: '8%',
-    borderWidth: 8,
-    borderRadius: 10,
-    borderColor: '#323547',
-    borderStartColor: '#323547',
-    justifyContent: 'center',
-    alignItems:'center',
-    alignSelf: 'center',
-    backgroundColor:'#323547',
-  },
+  // sol: {
+  //   marginTop: '14',
+  //   width: '40%',
+  //   height: '8%',
+  //   borderWidth: 8,
+  //   borderRadius: 10,
+  //   borderColor: '#323547',
+  //   borderStartColor: '#323547',
+  //   justifyContent: 'center',
+  //   alignItems:'center',
+  //   alignSelf: 'center',
+  //   backgroundColor:'#323547',
+  // },
   solText: {
     fontSize: 30,
     color: '#F1FAEE',
     fontWeight: '800'
+  },
+  container: {
+    marginTop: '14',
+    width:'90%',
+    height:'60%',
+    borderwidth: 8,
+    borderRadius: 10,
+    borderColor: '#323547',
+    borderStartColor: '#323547',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#323547',
+    flexDirection: 'row',
+  },
+  solHeader: {
+    height: '20%',
+    width: '90%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  solText:{
+    fontSize: 30,
+    justifyContent: 'center',
+    color: '#F1FAEE',
+  },
+  line: {
+    width: '90%',
+    height: '1%',
+    backgroundColor: '#F1FAEE',
+
+  },
+  containerInner: {
+    margin:1,
+    width: '90%',
+    height: '13%',
+  },
+  innerText: {
+    fontSize: '10',
+    justifyContent: 'center',
+    color: '#F1FAEE',
+
+  },
+  date: {
+    borderwidth: 8,
+    borderRadius: 10,
+    borderColor: '#323547',
+    borderStartColor: '#323547',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    height:'15%',
+    width: '70%'
+  },
+  dateText: {
+    fontSize: 20,
+    justifyContent: 'center',
+    color: '#F1FAEE',
   }
+
 });
