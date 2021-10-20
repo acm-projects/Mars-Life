c4f1ca15f34e68706aa49dfb079ffe03a7104e3f
rishixyz9 <reach2rishabh@rocketmail.com>
2021-10-20 17:58:07 -0500

    saddness :(

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import expandedNews from './expanded_news';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <expandedNews/>
     </View>
   );
 }
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#080B1F',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  loadText: {
+    color: "#FFFFFF"
+  }
 });
---
+++ expanded_news.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './basic.css';
+
+function expandedNews() {
+    return (
+        <div className="container">
+            <div className="navigation">
+                <div className="left-arrow">
+                    <h1 className="arrow"></h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default expandedNews;
