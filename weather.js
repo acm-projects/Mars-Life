f464067bcdc0270807a23d82ae8f05860db07c0f
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-10-20 01:21:53 -0500

    '

--- weather.js
+++ weather.js
@@ -7,83 +7,65 @@
  *
  */
  
- import  React from 'react';
+ import  React, { useState } from 'react';
  import { StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
 
- 
+ <script>
+  var a;
+  
+
+ </script>
 export default class weather extends React.Component{
  
+
+
   render(){
    return (
+     <View style = {style.body}>
+      <View style = {style.header}> 
+        <Text style = {style.headerText}>Weather</Text>
+      </View>
+      <Image 
+        style = {style.mars}
+        source = {require('./assets/Mars.png')}>
+      </Image>
+
+      <View style = {style.container}>
+        <View>
+
+        </View> 
+      </View>
+
+
+
+      <View>
+      {sol.map((item) =>
+        {return (
+          <View></View>
+
+
+        )}
+      
+      )}
+
+      </View>
+
+     </View>
 
    );
   }
 }
 const style = StyleSheet.create({
-  body: {
-    flex: 1,
-  },
-  header: {
-    marginTop: 10,
-    height: '10%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headerText: {
-    fontFamily: 'nunito',
-    fontSize: 50,
-    color: '#F1FAEE',
-  },
-  mars:{
-    justifyContent: 'center',
-    alignSelf: 'center',
-    resizeMode: 'contain',
-    height: '45%',
-    width: '65%'
-    },
-  container:{
-    width: '100%',
-    height: '43%',
-    padding: 5,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignContent: 'center',
-
-  },
-  select:{
-    width: '47%',
-    height: '48%',
-    borderWidth: 8,
-    borderRadius: 10,
-    borderColor: '#323547',
-    borderStartColor: '#323547',
-    margin: 5,
-  },
-  selectInner:{
-    flex: 1,
-    backgroundColor: '#323547',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  selectHeader:{
-    width: '100%',
-    height: '25%',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  selectText: {
-    fontSize: 30,
-    color: '#F1FAEE',
-    fontWeight: '800'
-  },
-  selectIcon:{
-    marginTop: 5,
-    height: '75%',
-    resizeMode: 'contain'
-  },
-  background:{
-    flex: 1,
-    resizeMode: 'stretch'
-  },
+  
 });
+
+
+const [sol, setSol] = useState([
+  { solNum: xfe, key: '-1' },
+  { solNum: xfe, key: '-2' },
+  { solNum: xfe, key: '-3' },
+  { solNum: xfe, key: '-4' },
+  { solNum: xfe, key: '-5' },
+  { solNum: xfe, key: '-6' },
+  { solNum: xfe, key: '-7' },
+]);
