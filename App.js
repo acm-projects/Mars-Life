d03e9c93d72653d11d4b57996122ccdca5f1a6df
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2021-10-21 23:05:45 -0500

    news page in progress inside app.js. made news.js file

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View, Image, ImageBackground, ScrollView, TouchableOpacity, Pressable, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'Sankalp', key: '1'},
+    { name: 'Batman', key: '2'},
+    { name: 'Superman', key: '3'},
+    { name: 'Flash', key: '4'},
+    { name: 'Spiderman', key: '5'},
+    { name: 'Ironman', key: '6'},
+    { name: 'Captain America', key: '7'}
+  ]);
+
+  //TouchableOpacity or TouchableHighlight (and withoutFeedback) can be used for clickable view
+
+  //------TO DO----------
+  // fix the border of each view so that it is aligned properly and overlaps 
+  // view. (to overlap just make the border bigger and rounder so make the view size smaller??)
+  // If statements don't work in JSX? Conditional rendering!
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <ScrollView>
+        { people.map(item => (
+          <View key={item.key} style={styles.roundedCorners}>
+            <TouchableOpacity onPress={null} >
+              <Text style={styles.item}> {item.name} </Text>
+            </TouchableOpacity>
+          </View>
+       ))}
+      </ScrollView>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#D0764E',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+  header: {
+    backgroundColor: '#ff5733'
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: '#E14616',
+    fontSize: 20,
+  },
+  topNews: {
+    //make the top article big
+  },
+  roundedCorners: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#E14616',
+  }
 });
+
+
+
+
+
+
+
+
+
+//----------original App.js----------
+
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
