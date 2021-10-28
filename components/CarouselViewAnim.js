380eb761113f39e255055d4f93908765e0c91dcc
syd-k <71054249+syd-k@users.noreply.github.com>
2021-10-27 21:58:26 -0500

    rover photos updated

--- components/CarouselViewAnim.js
+++ components/CarouselViewAnim.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text, StyleSheet, Dimensions, Image } from "react-native"
 export const SLIDER_WIDTH = Dimensions.get('window').width + 80
 export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
-
+// comment
 const CarouselCardItem = ({ item, index }) => {
   return (
     <View style={styles.container} key={index}>
