e95d9098dec57585acb9cbcd038742a2c8b8f4b5
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-10-30 12:29:22 -0500

    '

--- App.js
+++ App.js
@@ -8,26 +8,42 @@
  */
  
  import  React, { useState } from 'react';
- import { StyleSheet, Text, View, Image, ImageBackground, ScrollView} from 'react-native';
+ import { StyleSheet, Text, View, Image, ImageBackground, ScrollView } from 'react-native';
+ import * as Font from 'expo-font';
+ import {AppLoading} from 'expo';
  
-export default class weather extends React.Component{
+//  const getFonts = () => {
+//    return Font.loadAsync({
+//      'nunito-regular': require('./assets/Nunito-Regular.ttf')
+//    });
+//  }
  
+export default function App() {
+  const [loaded] = Font.useFonts({
+    Nunito: require('./assets/Nunito.ttf')
+  });
  
- 
-  render(){
+  if(!loaded){
+    return null;
+  }
    return (
     <ImageBackground
       style = {style.background}
       source = {require('./assets/HomeBack.png')}>
-      <ScrollView>
       <View style = {style.body}>
+ 
         <View style = {style.header}>
-          <Text style = {style.headerText}> Weather</Text>
+          <Text style = {style.headerText}>Weather</Text>
         </View>
+        <ScrollView>
  
           <Image
             style = {style.mars}
             source = {require('./assets/Mars.png')}></Image>
+ 
+            <View style = {style.solTemp}>
+              <Text style = {style.solTempText}>-20 F</Text>
+            </View>
            
             <View style = {style.sol}>
               <Text style = {style.solText}> Sol XXXX  </Text>
@@ -36,124 +52,159 @@ export default class weather extends React.Component{
             <View style = {style.date}>
               <Text style = {style.dateText}> 21 October 2021 </Text>
             </View>
-           
             <View style = {style.sideline}></View>
+ 
+           
             <View style = {style.container}>
               <View style = {style.week}>
                 <Text style = {style.weekText}> Sol XXXX-1</Text>
               </View>
               <Image style = {style.sun} source = {require('./assets/sun.png')}></Image>
-              <View style = {style.upline}></View>
-              <View style = {style.temp}>
-                <Text style = {style.tempText}></Text>
+              <View style = {style.week}>
+                <Text style = {style.weekText}>-sdfgsdfg sfdgsdfgr</Text>
               </View>
             </View>
  
-            <View style = {style.sideline}></View>
             <View style = {style.container}>
               <View style = {style.week}>
                 <Text style = {style.weekText}> Sol XXXX-2</Text>
               </View>
               <Image style = {style.sun} source = {require('./assets/sun.png')}></Image>
-              <View style = {style.upline}></View>
               <View style = {style.temp}>
-                <Text style = {style.tempText}></Text>
+                <Text style = {style.weekText}>-40</Text>
               </View>
             </View>
  
-            <View style = {style.sideline}></View>
             <View style = {style.container}>
               <View style = {style.week}>
                 <Text style = {style.weekText}> Sol XXXX-3</Text>
               </View>
               <Image style = {style.sun} source = {require('./assets/sun.png')}></Image>
-              <View style = {style.upline}></View>
               <View style = {style.temp}>
-                <Text style = {style.tempText}></Text>
+                <Text style = {style.weekText}>-40</Text>
               </View>
             </View>
  
-            <View style = {style.sideline}></View>
             <View style = {style.container}>
               <View style = {style.week}>
                 <Text style = {style.weekText}> Sol XXXX-4</Text>
               </View>
               <Image style = {style.sun} source = {require('./assets/sun.png')}></Image>
-              <View style = {style.upline}></View>
               <View style = {style.temp}>
-                <Text style = {style.tempText}></Text>
+                <Text style = {style.weekText}>-40</Text>
               </View>
             </View>
  
-            <View style = {style.sideline}></View>
             <View style = {style.container}>
               <View style = {style.week}>
                 <Text style = {style.weekText}> Sol XXXX-5</Text>
               </View>
               <Image style = {style.sun} source = {require('./assets/sun.png')}></Image>
-              <View style = {style.upline}></View>
               <View style = {style.temp}>
-                <Text style = {style.tempText}></Text>
+                <Text style = {style.weekText}>-40 F</Text>
               </View>
             </View>
  
-            <View style = {style.sideline}></View>
             <View style = {style.container}>
               <View style = {style.week}>
                 <Text style = {style.weekText}> Sol XXXX-6</Text>
               </View>
               <Image style = {style.sun} source = {require('./assets/sun.png')}></Image>
-              <View style = {style.upline}></View>
               <View style = {style.temp}>
-                <Text style = {style.tempText}></Text>
+                <Text style = {style.weekText}>-40</Text>
               </View>
             </View>
  
-            <View style = {style.sideline}></View>
             <View style = {style.container}>
               <View style = {style.week}>
                 <Text style = {style.weekText}> Sol XXXX-7</Text>
               </View>
               <Image style = {style.sun} source = {require('./assets/sun.png')}></Image>
-              <View style = {style.upline}></View>
               <View style = {style.temp}>
-                <Text style = {style.tempText}></Text>
+                <Text style = {style.weekText}>-60</Text>
               </View>
             </View>
-            <View style = {style.sideline}></View>
+            <View style = {style.sideline}/>
  
             <View style = {style.fact}>
-              <Text style = {style.factText}> Fun fact, it's almost always sunny on Mars.</Text>
+              <Text style = {style.factText}> Fun fact, it's literally always sunny on Mars.</Text>
             </View>
-            <View style = {style.sideline}></View>
-            <View style = {style.solData}>
-              <View style = {style.sunRise}>
+            <View style = {style.sideline}/>
  
+            <View style = {style.minMaxOuter}>
+              <View style = {style.minMaxInner}>
+                <Text style = {style.atmospheric}>Minimum Temp:</Text>
+                <Text style = {style.atmosphericNumber}>-80 F</Text>
+              </View>
+              <View style = {style.minMaxInner}>
+                <Text style = {style.atmospheric}>Maximum Temp:</Text>
+                <Text style = {style.atmosphericNumber}>-20 F</Text>
+              </View>
+            </View>
+            <View style = {style.sideline}/>
+ 
+            <View style = {style.minMaxOuter}>
+              <View style = {style.minMaxInner}>
+                <Text style = {style.atmospheric}>Ground Minimum Temp:</Text>
+                <Text style = {style.atmosphericNumber}>-80 F</Text>
+              </View>
+              <View style = {style.minMaxInner}>
+                <Text style = {style.atmospheric}>Ground Maximum Temp:</Text>
+                <Text style = {style.atmosphericNumber}>-20 F</Text>
               </View>
             </View>
  
+            <View style = {style.sideline}></View>
  
  
  
+            <View style = {style.minMaxOuter}>
+              <View style = {style.atmosphericInner}>
+                <Text style = {style.atmospheric}>Atmospheric Temperature:</Text>
+                <Text style = {style.atmosphericNumber}>-10 F</Text>
+              </View>
+            </View>
  
-          </View>
-      </ScrollView>
+            <View style = {style.sideline}/>
+ 
+            <View style = {style.minMaxOuter}>
+              <View style = {style.atmosphericInner}>
+                <Text style = {style.atmospheric}>Ultraviolet Index:</Text>
+                <Text style = {style.atmosphericNumber}>Ultraviolet</Text>
+              </View>
+            </View>
+ 
+            <View style = {style.sideline}/>
+ 
+            <View style = {style.minMaxOuter}>
+              <View style = {style.minMaxInner}>
+                <Text style = {style.atmospheric}>Sunrise Time:</Text>
+                <Text style = {style.atmosphericNumber}>8:00 AM</Text>
+              </View>
+              <View style = {style.minMaxInner}>
+                <Text style = {style.atmospheric}>Sunset Time:</Text>
+                <Text style = {style.atmosphericNumber}>8:00 PM</Text>
+              </View>
+            </View>
+            <View style = {style.sideline}/>
+            </ScrollView>
+      </View>
     </ImageBackground>
- 
    );
   }
-}
 const style = StyleSheet.create({
-    background:{
+  background: {
     flex: 1,
-    resizeMode: 'stretch'
+    resizeMode: 'stretch',
+    height: '100%',
+    width: '100%'
   },
   body: {
     flex: 1,
   },
   header: {
     marginTop: 20,
-    height: '10%',
+    paddingVertical: 15,
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
@@ -161,78 +212,72 @@ const style = StyleSheet.create({
   headerText: {
     fontSize: 50,
     color: '#F1FAEE',
+    fontFamily: 'Nunito'
   },
+ 
   mars:{
     justifyContent: 'center',
     alignSelf: 'center',
-    resizeMode: 'contain',
-    height : '20%',
-    width: '20%'
-   
   },
-  sol: {
-    marginTop: '14',
-    width: '40%',
-    height: '8%',
+  solTemp: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: '#323547',
     borderWidth: 8,
     borderRadius: 10,
     borderColor: '#323547',
-    borderStartColor: '#323547',
+    width: '40%',
+    marginBottom: 10
+  },
+  solTempText: {
+    fontSize: 55,
+    color: '#F1FAEE',
+    fontFamily: 'Nunito'
+  },
+  sol: {
+    marginTop: 1,
+    width: '40%',
+    borderWidth: 8,
+    borderRadius: 10,
+    borderColor: '#323547',
     justifyContent: 'center',
     alignItems:'center',
     alignSelf: 'center',
     backgroundColor:'#323547',
   },
+ 
   solText: {
     fontSize: 30,
     color: '#F1FAEE',
-    fontWeight: '800'
+    fontWeight: '800',
+    fontFamily: 'Nunito',
+    justifyContent: 'center'
   },
  
  
   container: {
-    marginTop: '14',
-    width:'90%',
-    height:'60%',
-    borderWidth: 8,
-    borderRadius: 10,
-    borderColor: '#323547',
-    borderStartColor: '#323547',
+    marginTop: 14,
+    width:'70%',
+    //borderWidth: 8,
+    //borderRadius: 10,
+    //borderColor: '#323547',
     justifyContent: 'center',
     alignSelf: 'center',
-    backgroundColor: '#323547',
-    flexDirection: 'row',
-  },
-  solHeader: {
-    height: '20%',
-    width: '90%',
-    alignSelf: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
+    //backgroundColor: '#323547',
+    flexDirection: 'row',
+    flex: 1
   },
+ 
   solText:{
     fontSize: 30,
-    justifyContent: 'center',
     color: '#F1FAEE',
+    fontFamily: 'Nunito'
   },
-  line: {
-    width: '90%',
-    height: '1%',
-    backgroundColor: '#F1FAEE',
  
-  },
-  containerInner: {
-    margin:1,
-    width: '90%',
-    height: '13%',
-  },
-  innerText: {
-    fontSize: 10,
-    justifyContent: 'center',
-    color: '#F1FAEE',
- 
-  },
   date: {
+    marginTop: 20,
     borderWidth: 8,
     borderRadius: 10,
     borderColor: '#323547',
@@ -240,19 +285,113 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
-    height:'15%',
-    width: '70%'
+    width: '50%',
+    backgroundColor: '#323547'
   },
   dateText: {
     fontSize: 20,
     justifyContent: 'center',
     color: '#F1FAEE',
+    fontFamily: 'Nunito'
   },
-  solData: {
+  sun: {
+    height: 35,
+    width: '20%',
+    justifyContent: 'center'
  
   },
-  sunRise: {}
+  week: {
+   // height: '10%',
+    width: '33.33%',
+    alignItems: 'center',
+    alignContent: 'center',
+    alignSelf: 'center'
+ 
+  },
+  weekText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    color: '#F1FAEE',
+    fontSize: 20,
+    fontFamily: 'Nunito'
+  },
+ 
+  temp: {
+    width:'40%'
+  },
+ 
+  sideline: {
+    marginTop: 10,
+    height: 1,
+    width: '70%',
+    backgroundColor: '#F1FAEE',
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+ 
+  fact: {
+    marginTop: 10,
+    marginBottom: 10,
+    // borderWidth: 8,
+    // borderRadius: 10,
+    // borderStartColor: '#323547',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '90%',
+  },
+ 
+  factText: {
+    fontSize: 20,
+    justifyContent: 'center',
+    color: '#F1FAEE',
+    fontFamily: 'Nunito'
+  },
+ 
+  minMaxOuter:{
+    paddingVertical: 5,
+    width: '85%',
+    borderWidth: 8,
+    borderRadius: 5,
+    borderColor: '#323547',
+    borderStartColor: '#323547',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#323547',
+    flexDirection: 'row',
+    flex: 1,
+    marginTop: 15,
+  },
+ 
+  minMaxInner: {
+    width: '50%',
+    height: '100%',
+    marginLeft:5    
+  },
+ 
+  atmosphericInner: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+ 
+  atmospheric: {
+    color: '#F1FAEE',
+    fontSize: 20,
+    fontFamily: 'Nunito'
+  },
+ 
+  atmosphericNumber:{
+    color: '#F1FAEE',
+    fontSize: 30,
+  }
+ 
  
 });
+ 
+ 
  
 
