97269bcff05ae1209fc06905394dd38c3e0826a9
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-10-30 12:20:39 -0500

    setting up navigation

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,4 +1,17 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import App from './screens/App.js';
+import weatherTest from './screens/weatherTest';
 
-const HomeStack = createStackNavigator({})
+const screens = {
+    homeScreen: {
+        screen: App
+    },
+    weatherScreen: {
+        screen: weatherTest
+    }
+}
+
+const HomeStack = createStackNavigator(screens)
+
+export default createAppContainer(HomeStack);
