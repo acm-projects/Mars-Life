4480a3495b8d136b484700aab976e0161e6d0cb8
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-10-30 13:45:44 -0500

    '

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -300,3 +300,32 @@ const style = StyleSheet.create({
    
    
 //   });
+
+
+
+
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ *
+ */
+ 
+ import  React, { useState } from 'react';
+ import { StyleSheet, Text, View, Image, ImageBackground, ScrollView } from 'react-native';
+ import * as Font from 'expo-font';
+ import {AppLoading} from 'expo';
+ import HomeScreen from './HomeScreen';
+
+export default function App() {
+
+   return (
+      <HomeScreen/>
+   );
+  }
+const style = StyleSheet.create({
+
+});
