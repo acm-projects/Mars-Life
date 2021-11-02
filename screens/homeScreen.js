6efc327604944268187a57a10dc19733f1de1134
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-02 03:18:41 -0500

    '

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -28,9 +28,9 @@ export default class HomeScreen extends React.Component{
  
   render(){
    return (
-     <ImageBackground
-     style = {style.background}
-     source = {require('./assets/Mars.png')}>
+    <ImageBackground
+    style = {style.background}
+    source = {require('./assets/HomeBack.png')}>
        <View style = {style.body}>
         <View style = {style.header}>
           <Text style = {style.headerText}> Mars Life </Text>
