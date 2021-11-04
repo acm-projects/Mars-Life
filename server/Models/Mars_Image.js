092cafa2f2a9e20e585c6d891cb4ccb2ff497b5d
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2021-11-04 15:57:03 -0500

    ...

--- server/Models/Mars_Image.js
+++ server/Models/Mars_Image.js
@@ -21,40 +21,10 @@ let data1 = []
         console.log(data)
       })
 
-// const options = {
-//     hostname: 'api.nasa.gov',
-//     path: '/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UsULBxdgulmBqWNLnbcbaBjoyEVDpUUbFcZbf1td',
-//     method: 'GET'
-// }
 
-// const req = https.request(options, res => {
-//     console.log(`statusCode: ${res.statusCode}`)
-  
-//     res.on('data', data => {
-//         dataList.push(data)
-//     }).on('end', () => {
-//         let data = JSON.parse(Buffer.concat(dataList))
-
-//         for(var i = 0; i < 10; i++) {
-//             imgList[i] = data.photos[i].img_src
-//             cameraName[i] = data.photos[i].camera.full_name
-//             earthDate[i] = data.photos[i].earth_date
-//         }
-
-//         for(var i = 0; i < 10; i++) {
-//             console.log(imgList[i])
-//             console.log(cameraName[i])
-//             console.log(earthDate[i])
-//         }
-//     })
-
-// })
-
-// req.on('error', error => {
-//     console.error(error)
-// })
-  
-// req.end()
+const refreshHandler = (key) => {            //print the key when an item is pressed
+  //console.log(key)
+}
 
 //---schema---
 const MarsImageSchema = new mongoose.Schema({
