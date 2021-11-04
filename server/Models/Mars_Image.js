c702669dfd0d02ab0120a3328419a95a42f1f0c8
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2021-11-04 15:35:00 -0500

    updated branch ss with new fetch api

--- server/Models/Mars_Image.js
+++ server/Models/Mars_Image.js
@@ -1,45 +1,64 @@
 const mongoose = require('mongoose')
-const https = require('https')
+//const https = require('https')
+//import {Agent} from 'https';
+const fetch = require("cross-fetch");
 
 var dataList = []
-let imgList = []
-let cameraName = []
-let earthDate = []
 
-const options = {
-    hostname: 'api.nasa.gov',
-    path: '/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UsULBxdgulmBqWNLnbcbaBjoyEVDpUUbFcZbf1td',
-    method: 'GET'
+let IMG_API = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UsULBxdgulmBqWNLnbcbaBjoyEVDpUUbFcZbf1td";
+
+let data1 = []
+    fetch(IMG_API)
+      .then(res => res.json())
+      .then(data => {
+          for(var i = 0; i < 10; i++){
+            dataList[i] = {
+                img_src : data.photos[i].img_src,
+                camera_name : data.photos[i].camera.full_name,
+                earth_date : data.photos[i].earth_date
+            }
+          }
+        console.log(data)
+      })
+
+const pressHandler = (key) => {            //print the key when an item is pressed
+  console.log(key)
 }
 
-const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`)
+// const options = {
+//     hostname: 'api.nasa.gov',
+//     path: '/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UsULBxdgulmBqWNLnbcbaBjoyEVDpUUbFcZbf1td',
+//     method: 'GET'
+// }
+
+// const req = https.request(options, res => {
+//     console.log(`statusCode: ${res.statusCode}`)
   
-    res.on('data', data => {
-        dataList.push(data)
-    }).on('end', () => {
-        let data = JSON.parse(Buffer.concat(dataList))
+//     res.on('data', data => {
+//         dataList.push(data)
+//     }).on('end', () => {
+//         let data = JSON.parse(Buffer.concat(dataList))
 
-        for(var i = 0; i < 10; i++) {
-            imgList[i] = data.photos[i].img_src
-            cameraName[i] = data.photos[i].camera.full_name
-            earthDate[i] = data.photos[i].earth_date
-        }
+//         for(var i = 0; i < 10; i++) {
+//             imgList[i] = data.photos[i].img_src
+//             cameraName[i] = data.photos[i].camera.full_name
+//             earthDate[i] = data.photos[i].earth_date
+//         }
 
-        for(var i = 0; i < 10; i++) {
-            console.log(imgList[i])
-            console.log(cameraName[i])
-            console.log(earthDate[i])
-        }
-    })
+//         for(var i = 0; i < 10; i++) {
+//             console.log(imgList[i])
+//             console.log(cameraName[i])
+//             console.log(earthDate[i])
+//         }
+//     })
 
-})
+// })
 
-req.on('error', error => {
-    console.error(error)
-})
+// req.on('error', error => {
+//     console.error(error)
+// })
   
-req.end()
+// req.end()
 
 //---schema---
 const MarsImageSchema = new mongoose.Schema({
@@ -51,8 +70,5 @@ const MarsImageSchema = new mongoose.Schema({
 mongoose.model("mars_image", MarsImageSchema)
 
 module.exports = {
-    dataList: dataList,
-    imgList: imgList,
-    cameraName: cameraName,
-    earthDate: earthDate,
+    dataList: dataList
 }
