c9e188c3cbffef7b38ddbc1a3d9bad0ced530cbf
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-09 17:44:52 -0600

    '

--- screens/weather.js
+++ screens/weather.js
@@ -36,10 +36,11 @@ export default function Weather() {
 
       <Modal visible = {modalOpen}>
         <ImageBackground
-        <View style = {style.modal}>
 
       style = {style.background}
       source = {require('../assets/HomeBack.png')}>
+              <View style = {style.modal}>
+
           <Text> test</Text>
           <TouchableOpacity>
            <Icons name={'arrow-back'} size={width*0.075} color='#000' style={style.button} onPress = {() => setModalOpen(false)}/>
