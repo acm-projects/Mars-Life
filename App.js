08be66b00f969878b9980d2e515302b9fe7e9d8b
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2021-11-08 18:04:21 -0600

    ...

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import News from './News.js';
+import News from './News';
 
 export default function App() {
   return (
