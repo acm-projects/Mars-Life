e2300780ae3ad9fa84be02d575d8a38b0dc72bac
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-10 16:24:49 -0600

    '

--- components/RoverPhotos.js
+++ components/RoverPhotos.js
@@ -92,7 +92,7 @@ const styles = StyleSheet.create({
     flex : 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#080B1F'
+  //  backgroundColor: '#080B1F'
   },
 
   photocontainer: {                        //how do I remove the that blue bar from top???
