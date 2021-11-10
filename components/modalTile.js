1462a463af41953cb8aa51cdd33ab592eff764fc
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-09 18:25:14 -0600

    '

--- components/modalTile.js
+++ components/modalTile.js
@@ -39,7 +39,8 @@ export default class modal extends React.Component{
 
 const style = StyleSheet.create({
   leftJustify: {
-    marginLeft: 'auto',
+    marginLeft: '20%',
+    width: '100%'
   },
   container:
   {
