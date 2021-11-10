cd09e792cbabb43bf9f294a448a6f25c3718b130
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-09 18:00:52 -0600

    '

--- screens/weather.js
+++ screens/weather.js
@@ -11,7 +11,7 @@ import  React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, Dimensions, Modal,} from 'react-native';
 import * as Font from 'expo-font';
 import WeatherTile from '../components/weatherTile';
-import ModalTlie from '../components/modalTile'; 
+import ModalTile from '../components/modalTile'; 
 import Icons from 'react-native-vector-icons/MaterialIcons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
