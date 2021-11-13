5efa80c32b0f35f6f7b71f2d40080b31df801acf
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-12 19:06:53 -0600

    '

--- screens/weather.js
+++ screens/weather.js
@@ -7,300 +7,296 @@
  *
  */
  
-import  React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, Dimensions, Modal,} from 'react-native';
-import * as Font from 'expo-font';
-import WeatherTile from '../components/weatherTile';
-import ModalTile from '../components/modalTile'; 
-import Icons from 'react-native-vector-icons/MaterialIcons';
-import SplashLoad from '../components/SplashLoad';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
-const {width, height} = Dimensions.get('window');
-
-export default function Weather() {
+ import  React, { useState } from 'react';
+ import { StyleSheet, Text, View, ImageBackground, ScrollView, Dimensions, Modal} from 'react-native';
+ import {LinearGradient} from 'expo-linear-gradient';
+ import * as Font from 'expo-font';
+ import WeatherTile from '../components/weatherTile';
+ import Icons from 'react-native-vector-icons/MaterialIcons';
+ import SplashLoad from '../components/SplashLoad';
+ import { TouchableOpacity } from 'react-native-gesture-handler';
+ 
+ const {width, height} = Dimensions.get('window');
+ 
+ export default function Weather() {
+    
    
+   const[modalOpen, setModalOpen] = useState(false);
+ 
+   const [loaded] = Font.useFonts({
+     Nunito: require('../assets/Nunito.ttf')
+   });
   
-  const[modalOpen, setModalOpen] = useState(false);
-
-  const [loaded] = Font.useFonts({
-    Nunito: require('../assets/Nunito.ttf')
-  });
+   // if(!loaded){
+   //   return <SplashLoad style={justifyContent='center'}/>
+   // }
  
-  // if(!loaded){
-  //   return <SplashLoad style={justifyContent='center'}/>
-  // }
-
-  return (
-    <ImageBackground
-      style = {style.background}
-      source = {require('../assets/HomeBack.png')}>
-
-
-
-      <Modal visible = {modalOpen}>
-        <ImageBackground style = {style.background} source = {require('../assets/Home2.png')}>
-
-          <View style = {style.modal}>
-
-            <ScrollView>
-              <ModalTile />
-            </ScrollView>
-          </View>
-        </ImageBackground>
-      </Modal>
-
-      <View style = {style.body}>
-        <ScrollView>
-        <WeatherTile />
-         
-          </ScrollView>
-
-      </View>
-    </ImageBackground>
-    
-   );
-  }
-const style = StyleSheet.create({
-  bottomBar:{
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-   // marginBottom: 30,
-    marginTop: height*0.055,
-    marginBottom: height * 0.055,
-  },
-  bottomInner:{
-    width: '20%',
-   // backgroundColor: 'white',
-    marginHorizontal: '2%'
-
-  },
-  bottomText: {
-   // color: 'white',
-  },
-  scroll: {
-    marginBottom: height * 0.055,
-  },
-
-
-  background: {
-    flex: 1,
-    resizeMode: 'stretch',
-    height: '100%',
-    width: '100%'
-  },
-  body: {
-    flex: 1,
-  },
-  header: {
-    marginTop: 20,
-    paddingVertical: 15,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    //flex:1,
-  },
-
-
-
+   return (
+     <View style={style.container2}>
+     <LinearGradient style={style.background} colors={['#D0764E', '#080B1F']}>
+       <View style = {style.body}>
+         <ScrollView>
+         <WeatherTile />
+          
+           </ScrollView>
+ 
+       </View>
+     </LinearGradient>
+     </View>
+    );
+   }
+ const style = StyleSheet.create({
+   bottomBar:{
+     flex: 1,
+     flexDirection: 'row',
+     justifyContent: 'center',
+     alignItems: 'center',
+    // marginBottom: 30,
+     marginTop: height*0.055,
+     marginBottom: height * 0.055,
+   },
+   bottomInner:{
+     width: '20%',
+    // backgroundColor: 'white',
+     marginHorizontal: '2%'
+ 
+   },
+   bottomText: {
+    // color: 'white',
+   },
+   scroll: {
+     marginBottom: height * 0.055,
+   },
+ 
+ 
+   background: {
+     flex: 1,
+     resizeMode: 'stretch',
+     height: '100%',
+     width: '100%'
+   },
+   body: {
+     flex: 1,
+   },
+   header: {
+     marginTop: 20,
+     paddingVertical: 15,
+     width: '100%',
+     justifyContent: 'center',
+     alignItems: 'center',
+     //flex:1,
+   },
+ 
+ 
+ 
+   
+   headerText: {
+     fontSize: 50,
+     color: '#F1FAEE',
+   //  fontFamily: 'Nunito'
+   },
+ 
+   animation: {
+     width: 200,
+     height: 200,
+     alignSelf: 'center',
+     justifyContent: 'center',
+     
+   },
   
-  headerText: {
-    fontSize: 50,
-    color: '#F1FAEE',
-  //  fontFamily: 'Nunito'
-  },
-
-  animation: {
-    width: 200,
-    height: 200,
-    alignSelf: 'center',
-    justifyContent: 'center',
-    
-  },
- 
-  mars:{
-    justifyContent: 'center',
-    alignSelf: 'center',
-  },
-  solTemp: {
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
-    backgroundColor: '#323547',
-    borderWidth: 8,
-    borderRadius: 10,
-    borderColor: '#323547',
-    width: '40%',
-    marginBottom: 10
-  },
-  solTempText: {
-    fontSize: 55,
-    color: '#F1FAEE',
-   fontFamily: 'Nunito'
-  },
-  sol: {
-    marginTop: 1,
-    width: '40%',
-    borderWidth: 8,
-    borderRadius: 10,
-    borderColor: '#323547',
-    justifyContent: 'center',
-    alignItems:'center',
-    alignSelf: 'center',
-    backgroundColor:'#323547',
-  },
- 
-  solText: {
-    fontSize: 30,
-    color: '#F1FAEE',
-    fontWeight: '800',
-   // fontFamily: 'Nunito',
-    justifyContent: 'center'
-  },
- 
- 
-  container: {
-    marginTop: 14,
-    width:'70%',
-    borderWidth: 8,
-    borderRadius: 10,
-    borderColor: '#323547',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    alignItems: 'center',
-    backgroundColor: '#323547',
-    flexDirection: 'row',
-    flex: 1
-  },
- 
-  solText:{
-    fontSize: 30,
-    color: '#F1FAEE',
-   fontFamily: 'Nunito'
-  },
- 
-  date: {
-    marginTop: 20,
-    borderWidth: 8,
-    borderRadius: 10,
-    borderColor: '#323547',
-    borderStartColor: '#323547',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    width: '50%',
-    backgroundColor: '#323547'
-  },
-  dateText: {
-    fontSize: 20,
-    justifyContent: 'center',
-    color: '#F1FAEE',
-   fontFamily: 'Nunito'
-  },
-  sun: {
-    height: 35,
-    width: '20%',
-    justifyContent: 'center'
- 
-  },
-  week: {
-    width: '33.33%',
-    alignItems: 'center',
-    alignContent: 'center',
-    alignSelf: 'center'
- 
-  },
-  temp: {
-    fontSize: 40,
-    width:'33%',
-    alignItems: 'center'
-  },
- 
-  weekText: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignContent: 'center',
-    color: '#F1FAEE',
-    fontSize: 20,
+   mars:{
+     justifyContent: 'center',
+     alignSelf: 'center',
+   },
+   solTemp: {
+     justifyContent: 'center',
+     alignSelf: 'center',
+     alignItems: 'center',
+     backgroundColor: '#323547',
+     borderWidth: 8,
+     borderRadius: 10,
+     borderColor: '#323547',
+     width: '40%',
+     marginBottom: 10
+   },
+   solTempText: {
+     fontSize: 55,
+     color: '#F1FAEE',
     fontFamily: 'Nunito'
+   },
+   sol: {
+     marginTop: 1,
+     width: '40%',
+     borderWidth: 8,
+     borderRadius: 10,
+     borderColor: '#323547',
+     justifyContent: 'center',
+     alignItems:'center',
+     alignSelf: 'center',
+     backgroundColor:'#323547',
+   },
+  
+   solText: {
+     fontSize: 30,
+     color: '#F1FAEE',
+     fontWeight: '800',
+    // fontFamily: 'Nunito',
+     justifyContent: 'center'
+   },
+  
+  
+   container: {
+     marginTop: 14,
+     width:'70%',
+     borderWidth: 8,
+     borderRadius: 10,
+     borderColor: '#323547',
+     justifyContent: 'center',
+     alignSelf: 'center',
+     alignItems: 'center',
+     backgroundColor: '#323547',
+     flexDirection: 'row',
+     flex: 1
+   },
+  
+   container2: {
+     flex: 1,
+     backgroundColor: '#D0764E',
+     // paddingTop: 40,
+     // paddingHorizontal: 20,
+     // alignItems: 'center',
+     // justifyContent: 'center',
+     flex: 1,
+     resizeMode: 'stretch',
+     overflow: 'hidden',
   },
  
-
-  sideline: {
-    marginTop: 10,
-    height: 1,
-    width: '70%',
-    backgroundColor: '#F1FAEE',
-    justifyContent: 'center',
-    alignSelf: 'center'
-  },
- 
-  fact: {
-    marginTop: 10,
-    marginBottom: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    width: '90%',
-  },
- 
-  factText: {
-    fontSize: 20,
-    justifyContent: 'center',
-    color: '#F1FAEE',
+   solText:{
+     fontSize: 30,
+     color: '#F1FAEE',
     fontFamily: 'Nunito'
-  },
- 
-  minMaxOuter:{
-    paddingVertical: 5,
-    width: '85%',
-    borderWidth: 8,
-    borderRadius: 5,
-    borderColor: '#323547',
-    borderStartColor: '#323547',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#323547',
-    flexDirection: 'row',
-    flex: 1,
-    marginTop: 15,
-  },
- 
-  minMaxInner: {
-    width: '50%',
-    height: '100%',
-    marginLeft:5    
-  },
- 
-  atmosphericInner: {
-    height: '100%',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
- 
-  atmospheric: {
-    color: '#F1FAEE',
-    fontSize: 20,
+   },
+  
+   date: {
+     marginTop: 20,
+     borderWidth: 8,
+     borderRadius: 10,
+     borderColor: '#323547',
+     borderStartColor: '#323547',
+     justifyContent: 'center',
+     alignItems: 'center',
+     alignSelf: 'center',
+     width: '50%',
+     backgroundColor: '#323547'
+   },
+   dateText: {
+     fontSize: 20,
+     justifyContent: 'center',
+     color: '#F1FAEE',
     fontFamily: 'Nunito'
-  },
+   },
+   sun: {
+     height: 35,
+     width: '20%',
+     justifyContent: 'center'
+  
+   },
+   week: {
+     width: '33.33%',
+     alignItems: 'center',
+     alignContent: 'center',
+     alignSelf: 'center'
+  
+   },
+   temp: {
+     fontSize: 40,
+     width:'33%',
+     alignItems: 'center'
+   },
+  
+   weekText: {
+     justifyContent: 'center',
+     alignItems: 'center',
+     alignContent: 'center',
+     color: '#F1FAEE',
+     fontSize: 20,
+     fontFamily: 'Nunito'
+   },
+  
  
-  atmosphericNumber:{
-    color: '#F1FAEE',
-    fontSize: 30,
-  },
-  button: {
-    overflow: 'hidden',
-    color: 'white',
-    backgroundColor: '#e36b35',
-    padding: width*.01,
-    borderColor: '#B35335',
-    borderRadius: 10,
-    borderWidth: 2,
-    alignSelf: 'center',
-    justifyContent: 'center'
-  },
-});
+   sideline: {
+     marginTop: 10,
+     height: 1,
+     width: '70%',
+     backgroundColor: '#F1FAEE',
+     justifyContent: 'center',
+     alignSelf: 'center'
+   },
+  
+   fact: {
+     marginTop: 10,
+     marginBottom: 10,
+     justifyContent: 'center',
+     alignItems: 'center',
+     alignSelf: 'center',
+     width: '90%',
+   },
+  
+   factText: {
+     fontSize: 20,
+     justifyContent: 'center',
+     color: '#F1FAEE',
+     fontFamily: 'Nunito'
+   },
+  
+   minMaxOuter:{
+     paddingVertical: 5,
+     width: '85%',
+     borderWidth: 8,
+     borderRadius: 5,
+     borderColor: '#323547',
+     borderStartColor: '#323547',
+     alignSelf: 'center',
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#323547',
+     flexDirection: 'row',
+     flex: 1,
+     marginTop: 15,
+   },
+  
+   minMaxInner: {
+     width: '50%',
+     height: '100%',
+     marginLeft:5    
+   },
+  
+   atmosphericInner: {
+     height: '100%',
+     width: '100%',
+     justifyContent: 'center',
+     alignItems: 'center'
+   },
+  
+   atmospheric: {
+     color: '#F1FAEE',
+     fontSize: 20,
+     fontFamily: 'Nunito'
+   },
+  
+   atmosphericNumber:{
+     color: '#F1FAEE',
+     fontSize: 30,
+   },
+   button: {
+     overflow: 'hidden',
+     color: 'white',
+     backgroundColor: '#e36b35',
+     padding: width*.01,
+     borderColor: '#B35335',
+     borderRadius: 10,
+     borderWidth: 2,
+     alignSelf: 'center',
+     justifyContent: 'center'
+   },
+ });
