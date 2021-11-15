e6fba9f8e36a52d9ddf7253019681b75f3b80cb2
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-14 20:47:56 -0600

    '

--- screens/weather.js
+++ screens/weather.js
@@ -86,8 +86,8 @@ const style = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    // marginTop: height*0.035,
-     marginBottom: height * 0.035,
+     marginTop: height*0.035,
+     marginBottom: height * 0.055,
     //backgroundColor: 'white'
     opacity:1
   },
