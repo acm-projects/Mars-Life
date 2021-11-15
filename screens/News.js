31c7a269cc3bac442597cb6c2a5fca9d882eef01
Arturo Yundt-Pacheco <ayundtp@gmail.com>
2021-11-14 21:22:10 -0600

    '

--- screens/News.js
+++ screens/News.js
@@ -59,10 +59,10 @@ const styles = StyleSheet.create({
     width: 100
   },
   topNewsText: {
+    color: "white",
     marginTop: 40,
     marginRight: 30,
     fontWeight: "bold",
-    color: "white",
   },
   backButton: {
     flex: 1,
@@ -90,12 +90,10 @@ const styles = StyleSheet.create({
     width: 65
   },
   newsText: {
+    color: "white",
     marginTop: 10,
     marginLeft: 80,
     fontWeight: "bold",
-    color: "white",
-
-
   },
 });
 
