9971a2225bfc95b100cb3833bb9cf970b9ed393f
Sankalp Shubham <87834473+sankalpshubham@users.noreply.github.com>
2021-11-16 19:32:38 -0600

    Images update better now

--- server/Models/Mars_Image.js
+++ server/Models/Mars_Image.js
@@ -11,8 +11,11 @@ let data1 = []
     fetch(IMG_API)
       .then(res => res.json())
       .then(data => {
-          for(var i = 0; i < 500; i++){
-            dataList[i] = {
+        console.log(data.photos.length);
+        let b = 0;
+          for(var i = 0; i < 2000; i++){
+            if (i % 5 == 0) 
+            dataList[b++] = {
                 img_src : data.photos[i].img_src,
                 camera_name : data.photos[i].camera.full_name,
                 earth_date : data.photos[i].earth_date
